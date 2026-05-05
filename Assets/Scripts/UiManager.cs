eaf5cfb1aff97d69d45eebb1be464ed0192ee605
SemantakCodes <semantak.bhandari10@gmail.com>
2026-05-05 22:11:15 +0545

    feat: gop gop completed

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -8,28 +8,44 @@ public class UiManager : MonoBehaviour
     [SerializeField] private Button quitButton;
     [SerializeField] private string gameScene;
     public GameObject donationBox;
+    [SerializeField] private AudioClip buttonClickSfx;
+    
+    private AudioSource audioSource;
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         playButton.onClick.AddListener(OnPlayButtonPressed);
         quitButton.onClick.AddListener(OnQuitButtonPressed);
     }
 
     private void OnPlayButtonPressed()
     {
+        PlayButtonSound();
         SceneManager.LoadScene(gameScene);
     }
 
     private void OnQuitButtonPressed()
     {
+        PlayButtonSound();
         Application.Quit();
+    }
+
+    private void PlayButtonSound()
+    {
+        if (audioSource != null && buttonClickSfx != null)
+        {
+            audioSource.PlayOneShot(buttonClickSfx);
+        }
     }
     public void OpenDonation()
     {
+        PlayButtonSound(); 
         donationBox.SetActive(true);
     }
     public void CloseDonation()
     {
+        PlayButtonSound();
         donationBox.SetActive(false);
     }
 
